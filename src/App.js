18a5557826459574817e48725c532d031a1be300
TatianaPri <87344989+Strecoza@users.noreply.github.com>
2023-11-05 21:53:29 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import {data} from './data';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [places, setPlaces]= useState(data);
+  const [showText, setShowText] = useState(false);
+  const removePlace = (id) => {
+    let newPlaces = places.filter(place => place.id !== id);
+    setPlaces(newPlaces)
+  }
+const showTextClick = (place) =>{
+    place.showMore = !place.showMore
+    setShowText(!showText)
+  }
+
+  return(
+  <div className="places">
+    <div className ='container'>
+      <h1>Top<span className="number"> {places.length} </span> places to visit <br/>in <span className="city">Los Angeles</span></h1>
     </div>
+
+    {places.map((place) => {
+    const {id, placeName, description, image, source, showMore} = place;
+
+      return(
+        <div  key = {id}>
+          <div className = 'container'>
+            <h2>{id}-{placeName}</h2>
+          </div>
+          <div className = 'container'>
+            <p>{showMore ? description : description.substring (0,200) + '..'}
+            <button onClick = {() => showTextClick (place)}> {showMore? 'Show less': 'Show more'}
+            </button></p>
+          </div>
+          <div className = 'container'>
+            <img src = {image} alt = 'Place to visit' width='500px'/>
+          </div>
+          <div className ='container'>
+            <p>source:  {source}</p>
+          </div>
+          <div className = 'container'>
+            <button className = 'btn' onClick= {() => removePlace(id)}> Remove </button>
+          </div>
+        </div>
+      )
+    })}
+ 
+  </div>
   );
 }
 
